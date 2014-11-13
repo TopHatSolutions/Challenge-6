4a403008cedc8053bb7b03f998ddec9a6e951839
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-13 16:13:09 -0600

    added missing email listing, routes, and views

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -6,4 +6,8 @@ class FinderController < ApplicationController
   def alphabetized
     @customers = Customer.all.order(full_name: :asc)
   end
+
+  def missing_email
+    @customers = Customer.all.where(email_address: :null)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'alphabetized' => 'finder#alphabetized'
 
+  get 'missing_email' => 'finder#missing_email'
+
   root to: 'finder#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
