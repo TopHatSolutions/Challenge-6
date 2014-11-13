201c5794960d290cf3a09ddb1b27370ef2395b5e
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-13 15:42:10 -0600

    Added alphabetical customer listing, routing, and view

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -2,4 +2,8 @@ class FinderController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def alphabetized
+    @customers = Customer.all.order(full_name: :asc)
+  end
 end
