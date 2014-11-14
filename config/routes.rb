cc952dde3ef02eed87ca2062f2ced498b6049cf7
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-13 19:24:30 -0600

    Created the provinces MVC bundle.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :provinces
+
   resources :customers
 
   get 'alphabetized' => 'finder#alphabetized'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112164656) do
+ActiveRecord::Schema.define(version: 20141114011554) do
 
   create_table "customers", force: true do |t|
     t.string   "full_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141112164656) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "province_id"
   end
 
 end
